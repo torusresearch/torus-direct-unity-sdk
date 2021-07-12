32688f60160ab2737e43168fb3d4dbd33f8fb963
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-07-12 20:58:34 +0700

    Convert code Kotlin

---
+++ ProjectPlugins/Android/torusdirect/src/main/java/org/torusresearch/unity/torusdirect/TorusDirectPlugin.kt
@@ -0,0 +1,56 @@
+@file:JvmName("TorusDirectPlugin")
+
+package org.torusresearch.unity.torusdirect
+
+import android.net.Uri
+import android.util.Log
+import androidx.browser.customtabs.CustomTabsIntent
+import com.unity3d.player.UnityPlayer
+import org.torusresearch.torusdirect.TorusDirectSdk
+import org.torusresearch.torusdirect.types.DirectSdkArgs
+import org.torusresearch.torusdirect.types.TorusNetwork
+
+private val instance = TorusDirect()
+
+public fun getInstance(): TorusDirect {
+    return instance
+}
+
+class TorusDirect internal constructor() {
+    companion object {
+        private val tag = "TorusDirectPlugin"
+    }
+
+    private var args: DirectSdkArgs? = null
+
+    fun init(browserRedirectUri: String, network: String, redirectUri: String) {
+        Log.d(
+            "${tag}#init",
+            arrayOf(
+                "browserRedirectUri=$browserRedirectUri",
+                "redirectUri=$redirectUri",
+                "network=$network"
+            ).joinToString(" ")
+        )
+        args = DirectSdkArgs(browserRedirectUri, TorusNetwork.valueOfLabel(network), redirectUri)
+    }
+
+    fun triggerLogin(typeOfLogin: String, verifier: String, clientId: String) {
+        if (args == null) throw Exception("TorusDirect.init must be called before triggerLogin.")
+        Log.d(
+            "${tag}#triggerLogin", arrayOf(
+                "typeOfLogin=$typeOfLogin",
+                "verifier=$verifier",
+                "clientId=$clientId",
+                "activity=${UnityPlayer.currentActivity.packageName}"
+            ).joinToString(" ")
+        )
+
+        val sdk = TorusDirectSdk(args, UnityPlayer.currentActivity)
+        Log.d(tag + "triggerLogin", "Initialized TorusDirect SDK successfully")
+
+        val customTabsBuilder = CustomTabsIntent.Builder()
+        val customTabsIntent = customTabsBuilder.build()
+        customTabsIntent.launchUrl(UnityPlayer.currentActivity, Uri.parse("https://customauth.io"))
+    }
+}

9b26ccef1e084b689bd61eea1990b784d186fb88
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-07-14 21:38:40 +0700

    Add prebuilt Twitch and Discord logins

--- Assets/Plugins/Torus/Scripts/TorusDirectMonoBehaviour.cs
+++ Assets/Plugins/Torus/Scripts/TorusDirectMonoBehaviour.cs
@@ -19,16 +19,29 @@ namespace Torus
 
     public class TorusDirectMonoBehaviour : MonoBehaviour
     {
+        [Serializable]
+        public class LoginConfig
+        {
+            public string verifier;
+            public string clientID;
+            public LoginConfig(string verifier, string clientID)
+            {
+                this.verifier = verifier;
+                this.clientID = clientID;
+            }
+        }
+
         public static TorusCredentials credentials { get; private set; }
         public static Exception exception { get; private set; }
 
         public TorusNetwork network = TorusNetwork.Testnet;
-        public string browserRedirectURI = "https://scripts.toruswallet.io/redirect.html";
+        public string browserRedirectURL = "https://scripts.toruswallet.io/redirect.html";
         public string appRedirectURI = "torusapp://org.torusresearch.torusdirectandroid/redirect";
-        public string googleVerifier = "google-lrc";
-        public string googleClientID = "221898609709-obfn3p63741l5333093430j3qeiinaa8.apps.googleusercontent.com";
-        public string facebookVerifier = "facebook-lrc";
-        public string facebookClientID = "617201755556395";
+
+        public LoginConfig loginWithGoogle = new LoginConfig("google-lrc", "221898609709-obfn3p63741l5333093430j3qeiinaa8.apps.googleusercontent.com");
+        public LoginConfig loginWithFacebook = new LoginConfig("facebook-lrc", "617201755556395");
+        public LoginConfig loginWithTwitch = new LoginConfig("twitch-lrc", "f5and8beke76mzutmics0zu4gw10dj");
+        public LoginConfig loginWithDiscord = new LoginConfig("discord-lrc", "682533837464666198");
 
         public UnityEvent onPreLogin;
         public UnityEvent<TorusEvent> onPostLogin;
@@ -36,7 +49,7 @@ namespace Torus
         public virtual void Awake()
         {
             TorusDirect.Init(
-                browserRedirectUri: new Uri(browserRedirectURI),
+                browserRedirectUri: new Uri(browserRedirectURL),
                 redirectUri: string.IsNullOrEmpty(appRedirectURI) ? null : new Uri(appRedirectURI),
                 network: network
             );
@@ -55,12 +68,22 @@ namespace Torus
 
         public void LoginWithGoogle()
         {
-            TriggerLogin(TorusTypeOfLogin.Google, googleVerifier, googleClientID);
+            TriggerLogin(TorusTypeOfLogin.Google, loginWithGoogle.verifier, loginWithGoogle.clientID);
         }
 
         public void LoginWithFacebook()
         {
-            TriggerLogin(TorusTypeOfLogin.Facebook, facebookVerifier, facebookClientID);
+            TriggerLogin(TorusTypeOfLogin.Facebook, loginWithFacebook.verifier, loginWithFacebook.clientID);
+        }
+
+        public void LoginWithTwitch()
+        {
+            TriggerLogin(TorusTypeOfLogin.Twitch, loginWithTwitch.verifier, loginWithTwitch.clientID);
+        }
+
+        public void LoginWithDiscord()
+        {
+            TriggerLogin(TorusTypeOfLogin.Discord, loginWithDiscord.verifier, loginWithDiscord.clientID);
         }
 
         public void __OnPreLogin__()
